f1cc445bf32816be533991768131eb12e1e0a6f6
Fathy Abdlhady <141967506+FathyAbdlhady@users.noreply.github.com>
2025-12-05 19:25:04 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
 export const TIMEOUT_SEC = 10;
 
@@ -9,3 +9,4 @@ export const KEY = '84ba9b20-137b-4baf-845a-72597ac04ed5';
 export const MODAL_CLOSE_SEC = 2;
 
 export const FORM_RE_RENDER_SEC = MODAL_CLOSE_SEC + 0.5;
+
